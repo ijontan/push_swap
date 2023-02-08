168e15f53cbb6981bdeeda143ce14a15e7424e87
ijon <itan@student.42kl.edu.my>
2023-02-08 21:17:41 +0800

    update

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: itan <itan@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 17:19:57 by itan              #+#    #+#             */
-/*   Updated: 2023/02/07 16:20:06 by itan             ###   ########.fr       */
+/*   Updated: 2023/02/08 17:31:40 by itan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,10 @@ int	main(int ac, char const **av)
 	t_list	*b;
 	int		i;
 	int		*content;
-	int		count;
 
-	count = 0;
 	i = 0;
-	b = (t_list *)malloc(sizeof(t_list));
+	b = NULL;
+	a = NULL;
 	while (i < ac - 1)
 	{
 		content = malloc(sizeof(int));
@@ -35,12 +34,11 @@ int	main(int ac, char const **av)
 		ft_lstadd_back(&a, ft_lstnew(content));
 	}
 	// radix_sort_ps(&a, &b, &count, ft_lstsize(a));
-	quick_sort(a, b, &count);
+	quick_sort(a, b, ft_lstsize(a), 0);
 	ft_printf("a:  ");
 	ft_lstiter(a, &put);
 	ft_printf("\nb:  ");
 	ft_lstiter(b, &put);
 	ft_printf("\n");
-	ft_printf("count:  %i\n", count);
 	return (0);
 }
--- src/sort/quick_sort.c
+++ src/sort/quick_sort.c
@@ -6,22 +6,41 @@
 /*   By: itan <itan@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 21:30:16 by itan              #+#    #+#             */
-/*   Updated: 2023/02/08 16:57:41 by itan             ###   ########.fr       */
+/*   Updated: 2023/02/08 17:32:08 by itan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	quick_sort(t_list *a, t_list *b, int top_len, int bot_len)
+int	partition(t_list *a, t_list *b, int bot_len)
 {
 	long	val;
+	int		nbot_len;
 
+	nbot_len = 0;
 	val = (long)(a->content);
-	a = a->next;
-	while (a)
+	while (bot_len > 0)
 	{
 		if ((long)(a->content) < val)
 			pa(&a, &b);
+		else
+		{
+			ra(&a);
+			nbot_len++;
+		}
 		a = a->next;
+		bot_len--;
 	}
+	return (nbot_len);
+}
+
+void	quick_sort(t_list *a, t_list *b, int top_len, int bot_len)
+{
+	int	nbot_len;
+
+	if (bot_len == 2)
+		return (sa(&a));
+	nbot_len = partition(a, b, bot_len);
+	quick_sort(a, b, bot_len - nbot_len, nbot_len);
+	quick_sort(a, b, 0, top_len);
 }
